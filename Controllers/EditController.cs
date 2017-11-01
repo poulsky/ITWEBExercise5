f9d2439f92f3d6ab6edd198af212ef8dd34a6b39
holmeren <soren_kh@hotmail.com>
2017-11-01 20:12:01 +0100

    Make ViewComponent

--- Controllers/EditController.cs
+++ Controllers/EditController.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ITWEBExercise5.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ITWEBExercise5.Controllers
 {
     public class EditController : Controller
     {
+        public List<string> cat = new List<string>();
+
         public IActionResult Index()
         {
+            
+            cat.Add("modstand");
+            cat.Add("resistor");
+            ViewBag.AllCategories = cat;
+            ViewBag.Selected = cat.FirstOrDefault();            
             return View();
         }
-    }
+    }    
 }
